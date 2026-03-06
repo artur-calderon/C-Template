b9b3dd833580e9fbb0b291cb84ceb3cac618cbb4
artur-calderon <artur.calderonart@gmail.com>
2026-03-06 18:12:20 -0300

    exercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int number;
+        int userNumber;
+        int tries = 0;
+        number = random.Next(1, 100);
+        bool answer = true;
+        do
+        {
+            tries += 1;
+            Console.WriteLine("Guess the number!");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber < number)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else if (userNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations! You guessed with {tries} tries");
+                Console.WriteLine("Do you want to play again? yes or no");
+                string response = Console.ReadLine();
+
+                if (response.ToLower() == "no")
+                {
+                    break;
+                }
+                else
+                {
+                    number = random.Next(1, 100);
+                }
+            }
+        } while (answer);
     }
 }
