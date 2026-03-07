b6ccfa17acd42db012c63c2153d4f6dffa76e228
artur-calderon <artur.calderonart@gmail.com>
2026-03-06 22:01:45 -0300

    exercise 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        int stop = 10;
+        int number = 0;
+        int total = 0;
+        double average = 0;
+        int max = 0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (stop != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+
+        }
+
+        int lowerPositive = int.MaxValue;
+
+        foreach (int num in numbers)
+        {
+            total += num;
+            if (num > 0 && num < lowerPositive)
+            {
+                lowerPositive = num;
+            }
+
+            if (max < num)
+            {
+                max = num;
+            }
+        }
+
+        average = (double)total / numbers.Count;
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The Average is {average}");
+        Console.WriteLine($"The largest number is {max}");
+        Console.WriteLine($"The smallest positive number is {lowerPositive}");
+
+        Console.WriteLine($"The Sorted List is: ");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
