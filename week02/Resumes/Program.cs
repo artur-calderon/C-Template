1a4b4ae24e0b205d267153fc90aea6b39feb64f4
artur-calderon <artur.calderonart@gmail.com>
2026-03-09 21:33:10 -0300

    resume exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+
+        job2._company = "Afirmeplay";
+        job2._jobTitle = "FullStack Engineer";
+        job2._startYear = 2025;
+        job2._endYear = 2026;
+
+
+        Resume resume1 = new Resume();
+
+        resume1._name = "Artur Calderon";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
