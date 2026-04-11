8342b12c78a6fba4415eb8c74ac84abeada2d50d
artur-calderon <artur.calderonart@gmail.com>
2026-04-11 15:59:41 -0300

    eternal quest done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Criar algumas formas simples (do jeito de um iniciante)
+        Square s = new Square("Red", 3.0);
+        Rectangle r = new Rectangle("Blue", 4.0, 2.5);
+        Circle c = new Circle("Green", 2.0);
+
+        // Testando GetColor() e GetArea() individualmente
+        Console.WriteLine("Square: color=" + s.GetColor() + ", area=" + s.GetArea());
+        Console.WriteLine("Rectangle: color=" + r.GetColor() + ", area=" + r.GetArea());
+        Console.WriteLine("Circle: color=" + c.GetColor() + ", area=" + c.GetArea());
+
+        // Colocar todas as formas em uma lista de Shape
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        Console.WriteLine();
+        Console.WriteLine("Percorrendo a lista de formas:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: " + shape.GetColor() + ", Area: " + shape.GetArea());
+        }
     }
 }
